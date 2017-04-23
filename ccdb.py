999c41562cc7f802747265173b40b7317cdd970d
Daniel Loffgren <daniel@loffgren.org>
2017-04-23 12:12:00 -0700

    Finished the rest of the implementation

--- ccdb.py
+++ ccdb.py
@@ -2,7 +2,7 @@
 
 import sys
 import json
-import os.path
+import os
 
 path = 'compile_commands.json'
 
@@ -14,4 +14,11 @@ else:
     fp = open(path, 'w')
     ccdb = []
 
+# Append current command
+this = dict([('directory', os.getcwd()), ('command', str(sys.argv)), ('file', sys.argv[-1])])
+ccdb.append(this)
+
+# Write out
 print ccdb
+json.write(ccdb, fp)
+fp.close()
